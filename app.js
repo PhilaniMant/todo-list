a9815eb144f4198235c4c4541a034741794fdbba
PhilaniMant <mancotywap@gmail.com>
2025-01-17 13:55:19 +0200

    added css and a help file

--- app.js
+++ app.js
@@ -6,18 +6,24 @@ const app = express();
 //set templating as ejs
 app.set('view engine', 'ejs')
 
+//static files
+app.use(express.static('public'));
+
 //route for  /
 app.get('/' , (req, res) => {
  res.render('home');
 });
 
-app.get('/about', (req, res) => {
-    res.render('about')
-});
-
 app.get('/front', (req, res) => {
     res.render('front')
 });
+
+app.get('/help', (req, res) => {
+    res.render('help')
+});
+
+
+
 
 //create a server 
 app.listen(3000,() => console.log('server is running'));
